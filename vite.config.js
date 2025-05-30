7fdc6da79834859721ec6acb563a996401931e35
Amelia <amelialx99@gmail.com>
2025-05-30 14:16:50 -0700

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/amelial9.github.io/',
+  base: process.env.NODE_ENV === 'production' ? '/amelial9.github.io/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
