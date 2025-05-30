2cbfe9d7942485d8dff1992064beff5c45dc5013
Amelia <amelialx99@gmail.com>
2025-05-30 03:58:39 -0700

    upload

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/amelial9.github.io/',
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
 })
