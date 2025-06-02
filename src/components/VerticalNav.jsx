a18949c094f4ad4cdb2e8cc3c87eb97c9cc04af8
Amelia <amelialx99@gmail.com>
2025-06-02 15:56:15 -0700

    feat: hamburger menu for mobile

--- src/components/VerticalNav.jsx
+++ src/components/VerticalNav.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VerticalNav.css';
+import portraitPic from '../assets/portrait.svg';
 
 const VerticalNav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="vertical-nav">
       <div className="logo">
         <h2>Amelia Li</h2>
       </div>
-      <div className="nav-links">
-        <a href="#about" className="nav-link">About</a>
-        <a href="#experiences" className="nav-link">Experiences</a>
-        <a href="#education" className="nav-link">Education</a>
-        <a href="#projects" className="nav-link">Projects</a>
-        <a href="https://notes.ameliali.com/" target="_blank" className="nav-link">Notes</a>
+      
+      <div className="menu-icon" onClick={toggleMenu}>
+        <img src={portraitPic} alt="menu" />
+      </div>
+
+      <div className={`nav-links ${isMenuOpen ? 'nav-links-open' : ''}`}>
+        <a href="#about" className="nav-link" onClick={() => setIsMenuOpen(false)}>About</a>
+        <a href="#experiences" className="nav-link" onClick={() => setIsMenuOpen(false)}>Experiences</a>
+        <a href="#education" className="nav-link" onClick={() => setIsMenuOpen(false)}>Education</a>
+        <a href="#projects" className="nav-link" onClick={() => setIsMenuOpen(false)}>Projects</a>
+        <a href="https://notes.ameliali.com/" target="_blank" className="nav-link" onClick={() => setIsMenuOpen(false)}>Notes</a>
       </div>
     </div>
   );
