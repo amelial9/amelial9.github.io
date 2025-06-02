9c8e4328bbe635859b68a16b139057ec51f9343d
Amelia <amelialx99@gmail.com>
2025-06-02 15:11:48 -0700

    Update VerticalNav.jsx

--- src/components/VerticalNav.jsx
+++ src/components/VerticalNav.jsx
@@ -10,6 +10,7 @@ const VerticalNav = () => {
       <div className="nav-links">
         <a href="#about" className="nav-link">About</a>
         <a href="#experiences" className="nav-link">Experiences</a>
+        <a href="#education" className="nav-link">Education</a>
         <a href="#projects" className="nav-link">Projects</a>
         <a href="https://notes.ameliali.com/" target="_blank" className="nav-link">Notes</a>
       </div>
