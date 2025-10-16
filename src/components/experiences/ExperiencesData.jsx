2dda5c301ab0a32fd5e3a540a20883b5121dea34
Amelia <amelialx99@gmail.com>
2025-10-15 23:52:31 -0700

    update img storage

--- src/components/experiences/ExperiencesData.jsx
+++ src/components/experiences/ExperiencesData.jsx
@@ -1,3 +1,7 @@
+import cursorLogo from '../../assets/logos/cursor.png';
+import ischoolLogo from '../../assets/logos/ischool.jpg';
+import goldenLabLogo from '../../assets/logos/goldenlab.png';
+
 export const experiencesData = [
   {
     date: 'Aug. 2025 - Present',
@@ -5,7 +9,7 @@ export const experiencesData = [
     subtitle: 'Cursor',
     description: 'Grow Cursor’s presence on campus',
     skills: ['Community Engagement', 'Project Management', 'Event Planning'],
-    logo: 'https://media.licdn.com/dms/image/v2/D560BAQEu4qyR3HdirA/company-logo_200_200/B56ZlyiWLvHkAI-/0/1758563230753/cursorai_logo?e=1761782400&v=beta&t=WQfO6IwOIXBJqNRz6IrkuDoTZvhI22JcdJPEoNlwNbc',
+    logo: cursorLogo,
   },
   {
     date: 'Mar. 2025 - Present',
@@ -13,7 +17,7 @@ export const experiencesData = [
     subtitle: 'University of Washington Information School',
     description: 'Leading weekly lab sessions for 30+ students, covering topics in database design, SQL, and data modeling',
     skills: ['SQL', 'Relational Databases', 'Data Modeling', 'Teaching & Mentorship'],
-    logo: 'https://pbs.twimg.com/profile_images/1811552266492760064/UgMNbZtf_400x400.jpg',
+    logo: ischoolLogo,
   },
   {
     date: 'Sep. 2024 - Present',
@@ -21,7 +25,7 @@ export const experiencesData = [
     subtitle: 'Golden Lab, Department of Neurobiology and Biophysics, University of Washington',
     description: 'Analyze physiological signal data from wireless implants; build and visualize machine learning models to detect behavioral patterns in animals.',
     skills: ['Python', 'Machine Learning', 'Data Visualization'],
-    logo: 'https://images.squarespace-cdn.com/content/v1/5b1b659871069912b3022368/1566324703483-F7Z3FI6BDLC9FQD5THZM/Golden+Lab+Color+Large.png?format=750w',
+    logo: goldenLabLogo,
   }
 ];
 
