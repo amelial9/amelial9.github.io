241f8ec57f9025627667a0d8736b19ae146c9d29
Amelia <amelial9@uw.edu>
2026-01-04 17:31:06 -0800

    customize cursor

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import catCursor from './assets/cursor/catcursor1-48.png'
+import catCursorHover from './assets/cursor/catcursor2-48.png'
+
+// Set cursor image as CSS variable with hotspot coordinates (24, 24 is center of 48x48)
+document.documentElement.style.setProperty('--cursor-image', `url(${catCursor}) 24 24`)
+document.documentElement.style.setProperty('--cursor-image-hover', `url(${catCursorHover}) 24 24`)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
