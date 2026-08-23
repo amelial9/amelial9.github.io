2ee8236b4a15f826bdf87f1523bb425b7c15af74
Amelia <amelial9@uw.edu>
2026-08-23 03:25:11 -0500

    update shelf

--- src/sections/shelf/Shelf.jsx
+++ src/sections/shelf/Shelf.jsx
@@ -6,9 +6,11 @@ import loki from '../../assets/shelf/loki.jpg';
 import siliconValley from '../../assets/shelf/siliconvalley.jpg';
 import missionImpossible4 from '../../assets/shelf/missionimpossible4.jpg';
 import topGun2 from '../../assets/shelf/topgun2.jpeg';
+import detectiveChinatown2 from '../../assets/shelf/detectivechinatown2.jpg';
+import goodbyeMrLoser from '../../assets/shelf/goodbyemrloser.jpg';
 
 export const showsLabel = "Shows I've spent the most time on";
-export const moviesLabel = 'Movies I rewatch too much';
+export const moviesLabel = 'Go-to files';
 
 export const shows = [
   { title: 'How I Met Your Mother', color: 'sand', poster: himym },
@@ -21,6 +23,8 @@ export const shows = [
 export const movies = [
   { title: 'Top Gun: Maverick', color: 'oat', poster: topGun2 },
   { title: 'Mission: Impossible – Ghost Protocol', color: 'cocoa', poster: missionImpossible4 },
+  { title: '唐人街探案2', color: 'sand', poster: detectiveChinatown2 },
+  { title: '夏洛特烦恼', color: 'fog', poster: goodbyeMrLoser },
 ];
 
 
